aa08076ee173b4352b9509252d37d0eaa17ddcdf
Robin Li <71902329+lirobinxc@users.noreply.github.com>
2021-04-13 23:07:50 -0400

    Update Navigation.js

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,11 +4,13 @@ import {ReactComponent as GithubLogo} from './github.svg'
 const Navigation = () => {
   return (
     <nav className="font-custom flex justify-end ">
-      <p className="flex py-5 px-5 border-solid text-gray-200 text-xl border-gray-700 hover:bg-gray-200 hover:text-gray-800
-        transition duration-100 cursor-pointer">
-        <GithubLogo className="fill-current h-7 pr-3"/>
-        lirobinxc
-      </p>
+      <a href="https://github.com/lirobinxc">  
+        <p className="flex py-5 px-5 border-solid text-gray-200 text-xl border-gray-700 hover:bg-gray-200 hover:text-gray-800
+          transition duration-100 cursor-pointer">
+            <GithubLogo className="fill-current h-7 pr-3"/>
+            lirobinxc
+        </p>
+      </a>
     </nav>
   )
 }
